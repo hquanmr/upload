12ae73a56e50ba42617f5dd2d6a86c3c4eab69a9
hquanmr <2976930785@qq.com>
2025-02-21 13:25:34 +0800

    1

--- stop.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-taskkill /F /IM php.exe > nul 2>&1
-echo 所有PHP进程已终止
-timeout /t 2 > nul
-exit
--- stop_servers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# 保存为 stop_servers.sh
-ps aux | grep -E 'php.*(channel|http|ws)_server.php' | grep -v grep | awk '{print $2}' | xargs kill -9
-echo -e "\033[31m[!] 所有服务已停止\033[0m"
